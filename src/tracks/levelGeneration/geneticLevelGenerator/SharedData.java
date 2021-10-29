d7fc0da6d66f379ce1b1e2e65922fd19ac9752cc
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2021-10-29 19:32:26 +1030

    Modify experimental parameters

--- src/tracks/levelGeneration/geneticLevelGenerator/SharedData.java
+++ src/tracks/levelGeneration/geneticLevelGenerator/SharedData.java
@@ -97,7 +97,7 @@ public class SharedData {
 	/**
 	 * The name of a the best agent with some human error
 	 */
-	public static final String AGENT_NAME = "tracks.singlePlayer.tools.sampleMCTS.Agent";
+	public static final String AGENT_NAME = "tracks.singlePlayer.advanced.sampleMCTS.Agent";
 	/**
 	 * The name of a naive agent
 	 */
